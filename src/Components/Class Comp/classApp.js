0a503474fec95e3d070f514ca1cb3be5b4259990
Maheswari2k1 <bmaheswari615@gmail.com>
2023-06-04 09:14:43 +0530

    Day-1 Changes

---
+++ src/Components/Class Comp/classApp.js
@@ -0,0 +1,7 @@
+import React,{Component} from "react";
+class ClassApp extends Component {
+    render(){
+        return <h1>I love Biryani</h1>;
+    }
+}
+export default ClassApp;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import ClassApp from "./Components/Class Comp/classApp";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<ClassApp />);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
